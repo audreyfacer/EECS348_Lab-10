360321d1cf1819a388134ef8c794aa44f054f792
Audrey <audfacer@ku.edu>
2025-04-16 11:18:38 -0500

    first upload kind of first draft

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,33 @@
 #include "calculator.hpp"
+#include <string>
+double parse_number(const std::string &expression){
+    int i = 0;
+    bool isNegative;
+    int len = expression.length();
+    if(expression[i]=='+'||expression[i]=='-'){
+        if(expression[i]=='-'){
+            isNegative = true;
+        }
+        i++;
+    }
+    double integerPart = 0.0;
+    while (i < len && expression[i] != '.') {
+        integerPart = integerPart * 10 + (expression[i] - '0');
+        i++;
+    }
+    double fractionalPart = 0.0;
+    if(i < len && expression[i]=='.'){
+        i++;
+        double divisor = 10.0;
+        while(i< len){
+            fractionalPart += (expression[i]-'0')/divisor;
+            divisor *= 10.0;
+            i++;
+        }
+
+    }
+    if(isNegative){
+        return -(integerPart+fractionalPart);
+    }
+    return integerPart+fractionalPart;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 #include "calculator.hpp"
-
+using namespace std;
 int main(int argc, char *argv[]) {
+    if (argc != 2)
+    {
+        cout << "Please provide the input file." << endl;
+        exit(1);
+    }
+    
+    ifstream inFile;
+    inFile.open(argv[1]);
+    if (inFile.is_open())
+    {
+        string str;
+        while(std::getline(inFile, str))
+        {
+            int len = str.length();
+            if(len == 0){
+                cout<<str<<"is invalid.";
+            }
+            int i = 0;
+
+            if(str[0]=='+'||str[0]=='-') i++;
+            bool digitsBeforeDot = false;
+            while(i < len && str[i] != '.'){
+                if (!(str[i] >= '0' && str[i] <= '9')) {
+                    cout<<str<<"is not a valid double.";
+                }
+                digitsBeforeDot = true;
+                i++;
+            }
+            if(str[i]=='.'){
+                i++;
+                while(i <len){
+                    if (!(str[i] >= '0' && str[i] <= '9')) {
+                        cout<<str<<"is not a valid double.";
+                    }
+                    i++;
+                }
+            }
+            parse_number(str)
+        }
+    }
+    else
+    {
+        cout << "Input file cannot be opened" << endl;
+        exit(1);
+    }
+    inFile.close();
+    return 0;
 
 }
